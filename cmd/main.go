17bd449bf63e79ebd57d43a9d2387f6c28cc7c9d
francomarin <franmarinmarin10@gmail.com>
2025-09-15 17:49:48 -0300

    add basic front #4

--- cmd/main.go
+++ cmd/main.go
@@ -4,16 +4,42 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gonum.org/v1/gonum/mat"
+
+	"github.com/matiasalvarado5/simplex-go/services"
 )
 
 func main() {
 	router := gin.Default()
+	router.LoadHTMLGlob("../templates/*")
 
-	//ruta de prueba mediante un json
+	// Página principal
 	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hola esta es la aplicacion web del metodo simplex",
+		c.HTML(http.StatusOK, "home.html", nil)
+	})
+
+	// Procesar formulario
+	router.POST("/solve", func(c *gin.Context) {
+		// Acá, por ahora, ponemos un problema de prueba fijo
+		A := mat.NewDense(2, 2, []float64{
+			2, 2,
+			1, 2,
+		})
+		b := []float64{6, 6}
+		cVec := []float64{3, 5}
+
+		result, err := services.SolveSimplex(A, b, cVec, true)
+		if err != nil {
+			c.HTML(http.StatusOK, "home.html", gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.HTML(http.StatusOK, "home.html", gin.H{
+			"resultado": result,
 		})
 	})
+
 	router.Run(":8080")
 }
